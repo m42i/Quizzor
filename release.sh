61f969dd8918a59375999ef5d006a4c70c9df5fb
m42i <mfourtytwoi@gmail.com>
2013-10-07 23:04:32 +0200

    Update release script

--- release.sh
+++ release.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # Package all necessary files and copy them along with the version.xml to the Dropbox folder
+version=$1
 
 zip -0 Quizzor.mmip Quizzor.vbs Install.ini Uninstall.ini license.txt 
-cp Quizzor.mmip version.xml ~/Dropbox/Public/Quizzor/
+cp Quizzor.mmip ~/Dropbox/Public/Quizzor/Quizzorv${version}.mmip
+cp version.xml ~/Dropbox/Public/Quizzor/version.xml
 
 # Create and copy german packages
 ./create_translated_packages.py -po -p Install_de.ini license.txt Uninstall.ini 
-cp Quizzor_de.mmip ~/Dropbox/Public/Quizzor/de/
+cp Quizzor_de.mmip ~/Dropbox/Public/Quizzor/de/Quizzorv${version}_de.mmip
 cp version_de.xml ~/Dropbox/Public/Quizzor/de/version.xml
